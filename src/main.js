f89be6c800bfaa3ca4276c547cc60473dbfa23a9
Aaron Ho <aayho@ucsc.edu>
2020-06-27 21:19:43 -0700

    assets and scenes

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
-let game = new Phaser.Game();
+let config = {
+    type: Phaser.CANVAS,
+    width: 640,
+    height: 480,
+    scene: [Menu, Play]
+  }
+
+// reserve keyboard vars
+let keyF, keyLEFT, keyRIGHT;
+
+  let game = new Phaser.Game(config);
