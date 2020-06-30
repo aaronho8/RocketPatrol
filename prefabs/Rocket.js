743a82b991d1db00223d4187304f2f350e7f6630
Aaron Ho <aayho@ucsc.edu>
2020-06-29 18:57:32 -0700

    Revert "Update Rocket.js"
    This reverts commit 8b6e04f54b88308912c42db9622546f9a31abb84.

--- prefabs/Rocket.js
+++ prefabs/Rocket.js
@@ -1,10 +1,11 @@
-// Rocket prefab
+/// Rocket prefab
 class Rocket extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame) {
         super(scene, x, y, texture, frame);
         scene.add.existing(this);   // add to existing scene, displayList, updateList
+        this.isFiring = false;      // tracks rocket's firing status
         this.sfxRocket = scene.sound.add('sfx_rocket'); // add rocket sfx
-        this.isFiring = false;      // track firing status
+
     }
 
     update() {
@@ -16,23 +17,27 @@ class Rocket extends Phaser.GameObjects.Sprite {
                 this.x += 2;
             }
         }
+
         // fire button
         if (Phaser.Input.Keyboard.JustDown(keyF) && !this.isFiring) {
             this.isFiring = true;
             this.sfxRocket.play();  // play sfx
         }
+
         // if fired, move up
         if (this.isFiring && this.y >= 108) {
             this.y -= 2;
         }
+
         // reset on miss
         if (this.y <= 108) {
             this.reset();
         }
     }
-    // reset rocket to "ground"
+    //reset rocket to "ground"
     reset() {
         this.isFiring = false;
         this.y = 431;
     }
+ 
 }
